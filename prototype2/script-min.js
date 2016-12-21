cd31568eab2bebf0d84f6d73c022253cfedb45df
xavierthomassmith <xave.smith@gmail.com>
2016-12-21 14:29:18 +1100

    Add files via upload

---
+++ prototype2/script-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".nav-bar-toggle").click(function(){$(".responsive-menu").toggle()})});
---
+++ prototype2/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.nav-bar-toggle').click(function(){
+      $('.responsive-menu').toggle();
+    });
+});
